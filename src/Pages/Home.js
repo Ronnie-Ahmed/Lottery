18ef882ef300bf4f943195bbc71481db328c2448
Ronnie-Ahmed <rksraisul@gmail.com>
2023-08-07 12:34:00 +0600

    fixedMargin

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -208,7 +208,7 @@ export const Home = () => {
       status === "disconnected" ||
       status === "disconnected" ||
       status === "connecting" ? (
-        <div className="flex items-center justify-center m-5 mx-4 px-1 md:mx-16 rounded-lg transform transition-all duration-300 shadow-2xl shadow-cyan-400 hover:scale-105 mb-8">
+        <div className="flex items-center mt-32 justify-center m-5 mx-4 px-1 md:mx-16 rounded-lg transform transition-all duration-300 shadow-2xl shadow-cyan-400 hover:scale-105 mb-8">
           <div className="flex flex-col items-center m-5">
             <img
               src={walletconnect}
